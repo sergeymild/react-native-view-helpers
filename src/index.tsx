22d6ec32e020bdcc471b312ccb42789d2616b223
Sergei Golishnikov <sergeymild@yandex.ru>
2026-06-13 11:01:40 +0300

    feat: flat barrel export of all helpers

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,26 @@
-export {};
+// alerts
+export { alert } from './alerts/alert';
+export { sheetAlert } from './alerts/bottom.sheet';
+export type {
+  BottomSheetAlertButton,
+  BottomSheetAlertButtonStyle,
+} from './alerts/bottom.sheet';
+
+// view helpers
+export {
+  viewHelpers,
+  type MeasureParams,
+  type MeasureTextResult,
+  type MeasureViewResult,
+} from './view-helpers';
+
+// status bar
+export { StatusBar, type StatusBarProps } from './status-bar/StatusBar';
+export { setSystemUIColor, setStatusBarStyle } from './status-bar/helpers';
+
+// lifecycle
+export {
+  subscribeOnAppLifecycle,
+  type AppLifecycleState,
+  type LifecycleSubscription,
+} from './lifecycle';
