958b23af92203b1c659f0a34a6937303d2e05118
Sergei Golishnikov <sergeymild@yandex.ru>
2026-06-13 11:25:39 +0300

    feat(android): convert LifecycleModule to TurboModule with onChange event

--- android/src/main/java/com/viewhelpers/LifecycleModule.kt
+++ android/src/main/java/com/viewhelpers/LifecycleModule.kt
@@ -2,35 +2,20 @@ package com.viewhelpers
 
 import android.os.Handler
 import android.os.Looper
-import androidx.annotation.NonNull
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
-import com.facebook.react.bridge.Arguments
 import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.WritableArray
-import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
-
 
 class LifecycleModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext), DefaultLifecycleObserver {
+  NativeLifecycleSpec(reactContext), DefaultLifecycleObserver {
 
-  override fun getName(): String {
-    return NAME
-  }
+  override fun getName(): String = NAME
 
   override fun initialize() {
     super.initialize()
     Handler(Looper.getMainLooper()).post {
       ProcessLifecycleOwner.get().lifecycle.addObserver(this)
-    }
-  }
-
-  override fun onCatalystInstanceDestroy() {
-    super.onCatalystInstanceDestroy()
-    Handler(Looper.getMainLooper()).post {
-      ProcessLifecycleOwner.get().lifecycle.removeObserver(this)
     }
   }
 
@@ -42,23 +27,15 @@ class LifecycleModule(reactContext: ReactApplicationContext) :
   }
 
   override fun onStart(owner: LifecycleOwner) {
-    sendEvent("active")
+    emitOnChange("active")
   }
 
   override fun onPause(owner: LifecycleOwner) {
-    sendEvent("inactive")
+    emitOnChange("inactive")
   }
 
   override fun onStop(owner: LifecycleOwner) {
-    sendEvent("background")
-  }
-
-  private fun sendEvent(type: String) {
-    if (reactApplicationContext == null) return
-    val array: WritableArray = Arguments.createArray()
-    array.pushString(type)
-    reactApplicationContext.getJSModule(RCTDeviceEventEmitter::class.java)
-      .emit("change", array)
+    emitOnChange("background")
   }
 
   companion object {
